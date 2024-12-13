764fe248d7dcd2244c0d7c0f82e19eaea304e43d
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2024-12-13 16:22:47 +0100

    Update MyCarousel.jsx

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,5 +1,23 @@
+import imgOne from "../assets/images/1a.png";
+import imgTwo from "../assets/images/1b.png";
+import imgThree from "../assets/images/1c.png";
+
 const MyCarousel = () => {
-  return;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 p-4">
+      <img src={imgOne} alt="album" className="rounded-lg" />
+      <img
+        src={imgTwo}
+        alt="album"
+        className=" hidden sm:inline-block rounded-lg"
+      />
+      <img
+        src={imgThree}
+        alt="album"
+        className=" hidden md:inline-block rounded-lg"
+      />
+    </div>
+  );
 };
 
 export default MyCarousel;
